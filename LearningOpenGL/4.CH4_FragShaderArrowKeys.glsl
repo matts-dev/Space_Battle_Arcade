bb26f58e5dd07e9bfc55409f56750bd832224ecd
dev-user <no.email>
2017-08-10 19:53:48 -0400

    finished texture challenge number 4, this one was really cool!

---
+++ LearningOpenGL/4.CH4_FragShaderArrowKeys.glsl
@@ -0,0 +1,16 @@
+#version 330 core
+
+in vec3 color;
+in vec2 texCoords;
+
+out vec4 fragColor;
+
+//uniform sampler2D textureData;
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+uniform float mixRatio;
+void main()
+{
+	fragColor = mix(texture(texture1, texCoords), texture(texture2, texCoords), mixRatio);
+}
---
+++ LearningOpenGL/4.TextureChallenge4.cpp
@@ -0,0 +1,127 @@
+///Challenge3 Try to display only the center pixels of the texture image on the rectangle in such a way that the individual pixels are getting visible by changing the texture coordinates. Try to set the texture filtering method to GL_NEAREST to see the pixels more clearly:
+#pragma once
+
+#include"Utilities.h"
+#include"Shader.h"
+#include"libraries/stb_image.h"
+
+
+namespace TexCH4 {
+	void pollArrowKeys(GLFWwindow * window, float& valueToControl);
+
+	int main() {
+		GLFWwindow* window = Utils::initOpenGl(800, 600);
+		if (!window) return -1;
+
+		//Special shader that allows uniforms to be set based on arrow keys
+		Shader shader3attribs("4.CH4_VertShaderArrowKeys.glsl", "4.CH4_FragShaderArrowKeys.glsl");
+
+		if (shader3attribs.createFailed()) { glfwTerminate(); return -1; }
+
+		static const float vertices[] = {
+			// positions          // colors           // texture coords
+			0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
+			0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+			-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+			-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
+		};
+
+		GLuint EAO, VAO, VBO;
+		Utils::generateRectForTextChallenge2(vertices, sizeof(vertices), EAO, VAO, VBO);
+
+
+		//LOAD TEXTURE DATA
+		int tWidth[2], tHeight[2], nrChannels[2];
+		unsigned char* data = stbi_load("Textures/container.jpg", &(tWidth[0]), &(tHeight[0]), &(nrChannels[0]), 0);
+		if (!data) { glfwTerminate(); return-1; }
+
+		GLuint Textures[2];
+		glGenTextures(2, Textures);
+		glBindTexture(GL_TEXTURE_2D, Textures[0]);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, tWidth[0], tHeight[0], 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		stbi_image_free(data);
+
+		data = stbi_load("Textures/awesomeface3.png", &(tWidth[1]), &(tHeight[1]), &(nrChannels[1]), 0);
+		if (!data) { glfwTerminate(); return-1; }
+
+		glBindTexture(GL_TEXTURE_2D, Textures[1]);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, tWidth[1], tWidth[1], 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_MIRRORED_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_MIRRORED_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		stbi_image_free(data);
+
+		shader3attribs.use();
+		GLuint texLoc1 = glGetUniformLocation(shader3attribs.getId(), "texture1");
+		GLuint texLoc2 = glGetUniformLocation(shader3attribs.getId(), "texture2");
+
+		//using GL_TEXTURE0 to reenforce that this is the number we're setting
+		glUniform1i(texLoc1, 0);
+		glUniform1i(texLoc2, GL_TEXTURE1 - GL_TEXTURE0);
+
+		GLuint mixRatioUniform = glGetUniformLocation(shader3attribs.getId(), "mixRatio");
+		float mixSetting = 0.5f;
+		glUniform1f(mixRatioUniform, mixSetting);
+
+		while (!glfwWindowShouldClose(window))
+		{
+
+			glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+
+			shader3attribs.use();
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, Textures[0]);
+
+			glUniform1f(mixRatioUniform, mixSetting);
+
+			glActiveTexture(GL_TEXTURE0 + 1);
+			glBindTexture(GL_TEXTURE_2D, Textures[1]);
+
+			glBindVertexArray(VAO);
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+
+			glfwPollEvents();
+			Utils::setWindowCloseOnEscape(window);
+			pollArrowKeys(window, mixSetting);
+			glfwSwapBuffers(window);
+		}
+
+		glDeleteVertexArrays(1, &VAO);
+		glDeleteBuffers(1, &VBO);
+		glDeleteBuffers(1, &EAO);
+
+		return 0;
+	}
+
+	void pollArrowKeys(GLFWwindow * window, float& valueToControl)
+	{
+		static const float incAmount = 0.0005f;
+		if (glfwGetKey(window, GLFW_KEY_UP) == GLFW_PRESS)
+		{
+			valueToControl += incAmount;
+		}
+		else if (glfwGetKey(window, GLFW_KEY_DOWN) == GLFW_PRESS)
+		{
+			valueToControl -= incAmount;
+		}
+	}
+}
+
+int main() {
+	return TexCH4::main();
+}
