29e8fb7ad8ed1c011d6d2abb4406cb34f8a909a9
dev-user <no.email>
2022-11-17 08:53:56 -0500

    STATE:NOT COMPILING - started project folder restructure to make setting up cmake easier. tons of include paths need fixing.

---
+++ Space_Battle_Arcade/Space_Battle_Arcade/Source/ReferenceCode/OpenGL/OpenGLAdvancedTopics/CubeMaps/CubeMaps_5EnvironmentMapping_Refraction.cpp
@@ -0,0 +1,397 @@
+#pragma once
+#include<iostream>
+
+#include<glad/glad.h> //include opengl headers, so should be before anything that uses those headers (such as GLFW)
+#include<GLFW/glfw3.h>
+#include <string>
+
+#include "Libraries/stb_image.h"
+#include "ReferenceCode/OpenGL/nu_utils.h"
+#include "ReferenceCode/OpenGL/Shader.h"
+#include "ReferenceCode/OpenGL/GettingStarted/Camera/CameraFPS.h"
+
+namespace
+{
+	CameraFPS camera(45.0f, -90.f, 0.f);
+
+	float lastFrameTime = 0.f;
+	float deltaTime = 0.0f;
+
+	float pitch = 0.f;
+	float yaw = -90.f;
+
+	float FOV = 45.0f;
+
+	/* Common refractive indices
+	Material	Refractive index
+		Air			1.00
+		Water		1.33
+		Ice			1.309
+		Glass		1.52
+		Diamond		2.42
+	*/
+
+	const char* vertex_shader_src = R"(
+				#version 330 core
+				layout (location = 0) in vec3 position;				
+				layout (location = 1) in vec3 normalData;
+				
+				out vec3 interpNormal;
+				out vec3 worldPos;
+				
+				uniform mat4 model;
+				uniform mat4 view;
+				uniform mat4 projection;
+
+				void main(){
+		
+					interpNormal = normalize(mat3(transpose(inverse(model))) * normalData);
+					worldPos = vec3(model * vec4(position, 1.0f));
+
+					gl_Position = projection * view * model * vec4(position, 1);
+				}
+			)";
+	const char* frag_shader_src = R"(
+				#version 330 core
+				out vec4 fragmentColor;
+
+				in vec3 interpNormal;
+				in vec3 worldPos;
+
+				uniform samplerCube environment;
+				uniform vec3 cameraPos;
+				
+				void main(){
+					float refractiveRatioAirToGlass = 1.0f / 1.52f;
+
+					vec3 normal = normalize(interpNormal);
+					vec3 incident = worldPos - cameraPos;
+					vec3 refractionDir = refract(incident, normal, refractiveRatioAirToGlass);
+
+					fragmentColor = vec4(texture(environment, refractionDir).rgb, 1.0f);
+				}
+			)";
+
+	const char* skybox_vertex_shader_src = R"(
+				#version 330 core
+				layout (location = 0) in vec3 position;				
+				layout (location = 1) in vec2 inTexCoord;
+				
+				out vec3 texCoord;
+				
+				uniform mat4 view_without_translation;
+				uniform mat4 projection;
+
+				void main(){
+					texCoord = position;
+					gl_Position = projection * view_without_translation * vec4(position, 1);
+
+					//configure position so depth will be maximum value
+					//when this is sent out of vertex shader, it will do a perspective division of w/w, which will be 1 (the maximum depth in NDC). 
+					gl_Position = gl_Position.xyww; 
+				}
+			)";
+
+	const char* skybox_frag_shader_src = R"(
+				#version 330 core
+				out vec4 fragmentColor;
+
+				in vec3 texCoord;
+
+				uniform samplerCube skybox;
+				
+				void main(){
+					fragmentColor = texture(skybox, texCoord);
+				}
+			)";
+
+
+	void processInput(GLFWwindow* window)
+	{
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		{
+			glfwSetWindowShouldClose(window, true);
+		}
+		camera.handleInput(window, deltaTime);
+	}
+
+	void true_main()
+	{
+		camera.setPosition(0.0f, 0.0f, 3.0f);
+		int width = 800;
+		int height = 600;
+
+		GLFWwindow* window = init_window(width, height);
+
+		glViewport(0, 0, width, height);
+		glfwSetFramebufferSizeCallback(window, [](GLFWwindow*window, int width, int height) {  glViewport(0, 0, width, height); });
+		camera.exclusiveGLFWCallbackRegister(window);
+
+		glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+
+		//-------------------------------------CUBE MAP TEXTURE-------------------------------------
+		float skyboxVertices[] = {
+			// positions          
+			-1.0f,  1.0f, -1.0f,
+			-1.0f, -1.0f, -1.0f,
+			1.0f, -1.0f, -1.0f,
+			1.0f, -1.0f, -1.0f,
+			1.0f,  1.0f, -1.0f,
+			-1.0f,  1.0f, -1.0f,
+
+			-1.0f, -1.0f,  1.0f,
+			-1.0f, -1.0f, -1.0f,
+			-1.0f,  1.0f, -1.0f,
+			-1.0f,  1.0f, -1.0f,
+			-1.0f,  1.0f,  1.0f,
+			-1.0f, -1.0f,  1.0f,
+
+			1.0f, -1.0f, -1.0f,
+			1.0f, -1.0f,  1.0f,
+			1.0f,  1.0f,  1.0f,
+			1.0f,  1.0f,  1.0f,
+			1.0f,  1.0f, -1.0f,
+			1.0f, -1.0f, -1.0f,
+
+			-1.0f, -1.0f,  1.0f,
+			-1.0f,  1.0f,  1.0f,
+			1.0f,  1.0f,  1.0f,
+			1.0f,  1.0f,  1.0f,
+			1.0f, -1.0f,  1.0f,
+			-1.0f, -1.0f,  1.0f,
+
+			-1.0f,  1.0f, -1.0f,
+			1.0f,  1.0f, -1.0f,
+			1.0f,  1.0f,  1.0f,
+			1.0f,  1.0f,  1.0f,
+			-1.0f,  1.0f,  1.0f,
+			-1.0f,  1.0f, -1.0f,
+
+			-1.0f, -1.0f, -1.0f,
+			-1.0f, -1.0f,  1.0f,
+			1.0f, -1.0f, -1.0f,
+			1.0f, -1.0f, -1.0f,
+			-1.0f, -1.0f,  1.0f,
+			1.0f, -1.0f,  1.0f
+		};
+
+		GLuint skyboxVAO;
+		glGenVertexArrays(1, &skyboxVAO);
+		glBindVertexArray(skyboxVAO);
+
+		GLuint skyboxVBO;
+		glGenBuffers(1, &skyboxVBO);
+		glBindBuffer(GL_ARRAY_BUFFER, skyboxVBO);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(skyboxVertices), skyboxVertices, GL_STATIC_DRAW);
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), reinterpret_cast<void*>(0));
+		glEnableVertexAttribArray(0);
+		glBindVertexArray(0);
+
+		std::vector<std::string> faces = {
+			"Textures/skybox1/right.jpg",
+			"Textures/skybox1/left.jpg",
+			"Textures/skybox1/top.jpg",
+			"Textures/skybox1/bottom.jpg",
+			"Textures/skybox1/front.jpg",
+			"Textures/skybox1/back.jpg"
+		};
+
+		GLuint cubeMapTexID;
+		glGenTextures(1, &cubeMapTexID);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, cubeMapTexID);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
+
+		stbi_set_flip_vertically_on_load(false);
+		int wid, hgt, numChannels;
+		for (size_t i = 0; i < faces.size(); ++i)
+		{
+			unsigned char* data = stbi_load(faces[i].c_str(), &wid, &hgt, &numChannels, 0);
+			if (data)
+			{
+				glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, wid, hgt, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+			}
+			else
+			{
+				std::cerr << "failed to load cube map face" << std::endl;
+				exit(-1);
+			}
+			stbi_image_free(data);
+		}
+
+		/* These are defined in sequential order and thus can be incremented in a loop
+		GL_TEXTURE_CUBE_MAP_POSITIVE_X	Right
+		GL_TEXTURE_CUBE_MAP_NEGATIVE_X	Left
+		GL_TEXTURE_CUBE_MAP_POSITIVE_Y	Top
+		GL_TEXTURE_CUBE_MAP_NEGATIVE_Y	Bottom
+		GL_TEXTURE_CUBE_MAP_POSITIVE_Z	Back
+		GL_TEXTURE_CUBE_MAP_NEGATIVE_Z	Front
+		*/
+
+
+		//--------------------------------------------------------------------------
+
+		float vertices[] = {
+			//px    py      pz     nx    ny     nz
+			-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+			0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+			0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+			0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+			-0.5f,  0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+			-0.5f, -0.5f, -0.5f,  0.0f,  0.0f, -1.0f,
+
+			-0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+			0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+			0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+			0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+			-0.5f,  0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+			-0.5f, -0.5f,  0.5f,  0.0f,  0.0f, 1.0f,
+
+			-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+			-0.5f,  0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+			-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+			-0.5f, -0.5f, -0.5f, -1.0f,  0.0f,  0.0f,
+			-0.5f, -0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+			-0.5f,  0.5f,  0.5f, -1.0f,  0.0f,  0.0f,
+
+			0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+			0.5f,  0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+			0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+			0.5f, -0.5f, -0.5f,  1.0f,  0.0f,  0.0f,
+			0.5f, -0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+			0.5f,  0.5f,  0.5f,  1.0f,  0.0f,  0.0f,
+
+			-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+			0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+			0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+			0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+			-0.5f, -0.5f,  0.5f,  0.0f, -1.0f,  0.0f,
+			-0.5f, -0.5f, -0.5f,  0.0f, -1.0f,  0.0f,
+
+			-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+			0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f,
+			0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+			0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+			-0.5f,  0.5f,  0.5f,  0.0f,  1.0f,  0.0f,
+			-0.5f,  0.5f, -0.5f,  0.0f,  1.0f,  0.0f
+		};
+
+		GLuint vao;
+		glGenVertexArrays(1, &vao);
+		glBindVertexArray(vao);
+
+		GLuint vbo;
+		glGenBuffers(1, &vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), reinterpret_cast<void*>(0));
+		glEnableVertexAttribArray(0);
+
+		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), reinterpret_cast<void*>(3 * sizeof(float)));
+		glEnableVertexAttribArray(1);
+
+		glBindVertexArray(0);
+
+		Shader skyboxShader(skybox_vertex_shader_src, skybox_frag_shader_src, false);
+		skyboxShader.use();
+		skyboxShader.setUniform1i("skybox", 0);
+
+		Shader shader(vertex_shader_src, frag_shader_src, false);
+		shader.use();
+
+		//inform shader which texture units that its samplers should be bound to
+		shader.setUniform1i("environment", 0);
+
+
+		glm::vec3 cubePositions[] = {
+			glm::vec3(0.0f,  0.0f,  0.0f),
+			glm::vec3(2.0f,  5.0f, -15.0f),
+			glm::vec3(-1.5f, -2.2f, -2.5f),
+			glm::vec3(-3.8f, -2.0f, -12.3f),
+			glm::vec3(2.4f, -0.4f, -3.5f),
+			glm::vec3(-1.7f,  3.0f, -7.5f),
+			glm::vec3(1.3f, -2.0f, -2.5f),
+			glm::vec3(1.5f,  2.0f, -2.5f),
+			glm::vec3(1.5f,  0.2f, -1.5f),
+			glm::vec3(3.f,  0.0f, 0.0f)
+		};
+
+		glEnable(GL_DEPTH_TEST);
+		while (!glfwWindowShouldClose(window))
+		{
+			float currentTime = static_cast<float>(glfwGetTime());
+			deltaTime = currentTime - lastFrameTime;
+			lastFrameTime = currentTime;
+
+			processInput(window);
+
+			glm::mat4 view = camera.getView();
+			glm::mat4 projection = glm::perspective(glm::radians(FOV), static_cast<float>(width) / height, 0.1f, 100.0f);
+
+			glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+			//draw scene
+			shader.use();
+			shader.setUniform3f("cameraPos", camera.getPosition());
+			glBindVertexArray(vao);
+
+			//set texture unit 0 to be the background
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, cubeMapTexID);
+
+			for (size_t i = 0; i < sizeof(cubePositions) / sizeof(glm::vec3); ++i)
+			{
+				glm::mat4 model;
+				float angle = 20.0f * i;
+				model = glm::translate(model, cubePositions[i]);
+				model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+
+
+				shader.setUniformMatrix4fv("model", 1, GL_FALSE, glm::value_ptr(model));
+				shader.setUniformMatrix4fv("view", 1, GL_FALSE, glm::value_ptr(view));  //since we don't update for each cube, it would be more efficient to do this outside of the loop.
+				shader.setUniformMatrix4fv("projection", 1, GL_FALSE, glm::value_ptr(projection));
+
+				glBindVertexArray(vao);
+				glDrawArrays(GL_TRIANGLES, 0, 36);
+			}
+
+
+			//draw skybox last
+			glDepthFunc(GL_LEQUAL);
+			skyboxShader.use();
+
+			//trim off translation rows from the view 
+			glm::mat4 trimmedView = glm::mat4(glm::mat3(view));
+
+			skyboxShader.setUniformMatrix4fv("view_without_translation", 1, GL_FALSE, glm::value_ptr(trimmedView));
+			skyboxShader.setUniformMatrix4fv("projection", 1, GL_FALSE, glm::value_ptr(projection));
+
+			glBindVertexArray(skyboxVAO);
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_CUBE_MAP, cubeMapTexID);
+			constexpr unsigned int num_verts = sizeof(skyboxVertices) / 3;
+			glDrawArrays(GL_TRIANGLES, 0, num_verts);
+			glDepthFunc(GL_LESS);
+			glfwSwapBuffers(window);
+			glfwPollEvents();
+
+		}
+
+		glfwTerminate();
+		glDeleteVertexArrays(1, &vao);
+		glDeleteBuffers(1, &vbo);
+
+		glDeleteVertexArrays(1, &skyboxVAO);
+		glDeleteBuffers(1, &skyboxVBO);
+	}
+}
+
+//int main()
+//{
+//	true_main();
+//}
