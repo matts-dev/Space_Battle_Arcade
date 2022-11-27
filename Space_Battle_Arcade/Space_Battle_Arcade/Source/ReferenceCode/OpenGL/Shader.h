af93039e7469f0a7c473122db1945e86f81de2ac
dev-user <no.email>
2022-11-27 14:31:16 -0500

    Adding missed file after GLM include path changes.

--- Space_Battle_Arcade/Space_Battle_Arcade/Source/ReferenceCode/OpenGL/Shader.h
+++ Space_Battle_Arcade/Space_Battle_Arcade/Source/ReferenceCode/OpenGL/Shader.h
@@ -3,7 +3,7 @@
 #include<memory>
 #include<vector>
 
-#include<glad/glad.h> 
+#include <glad/glad.h> 
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
